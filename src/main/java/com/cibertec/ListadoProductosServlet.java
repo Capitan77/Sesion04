8055cef037065feb9948e90600fdebcc66027c74
Capitan77 <dael202106@gmail.com>
2024-11-30 12:06:44 -0500

    Inteface product

--- src/main/java/com/cibertec/ListadoProductosServlet.java
+++ src/main/java/com/cibertec/ListadoProductosServlet.java
@@ -1,5 +1,8 @@
 package com.cibertec;
 
+import com.cibertec.model.Producto;
+import com.cibertec.model.dao.ProductoDao;
+import com.cibertec.model.dao.impl.ProductoDaoImpl;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -19,37 +22,22 @@ import java.util.List;
 @WebServlet(name = "ListadoProductosServlet", urlPatterns = "/ListadoProductosServlet")
 public class ListadoProductosServlet extends HttpServlet {
 
+    ProductoDao productoDao = new ProductoDaoImpl();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 
-        List<String[]> productos = new ArrayList<>();
 
-        try(
-                Connection connection = DBConnection.getConnector();
-                Statement statement = connection.createStatement();
-                ResultSet resultSet =statement.executeQuery("SELECT id, nombre, precio, stock, descuento FROM Productos")
-                ) {
-
-                while (resultSet.next()) {
-                    String[] item = new String[5];
-                    item[0] = String.valueOf(resultSet.getInt("id"));
-                    item[1] = resultSet.getString("nombre");
-                    item[2] = String.valueOf(resultSet.getDouble("precio"));
-                    item[3] = String.valueOf(resultSet.getInt("stock"));
-                    item[4] = String.valueOf(resultSet.getInt("descuento"));
-                    productos.add(item);
-                }
+        try {
+            List<Producto> productos = productoDao.listarProducto();
 
 
-        } catch (SQLException | ClassNotFoundException error) {
-            throw new ServletException(
-                    "Error al listar los productos",
-                    error
-            );
+            request.setAttribute("listaProductos", productos);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+            dispatcher.forward(request, resp);
+        }catch (SQLException error){
+            throw new ServletException("Error al obtener la lista", error);
         }
 
-        request.setAttribute("listaProductos", productos);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
-        dispatcher.forward(request, resp);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ page import="java.util.List" %>
+<%@ page import="com.cibertec.model.Producto"%>
 
 <!DOCTYPE html>
 <html>
@@ -13,12 +14,12 @@
         <a href="registro.jsp" class="btn btn-success mb-3">Agregar nuevo producto</a>
         <!-- Bloque listado de productos -->
 
-        <% List<String[]> items = (List<String[]>) request.getAttribute("listaProductos"); %>
+        <% List<Producto> items = (List<Producto>) request.getAttribute("listaProductos"); %>
 
-        <% for (String[] item : items) { %>
+        <% for (Producto item : items) { %>
 
             <div class="col-md-4">
-                <%= item[1] %>
+                <%= item.getNombre() %>
             </div>
 
         <% } %>
